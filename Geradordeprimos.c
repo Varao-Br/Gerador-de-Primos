51d45c829f0197fe99fecfe7673bfc310905d5ca
Anderson André Ferreira Gomes <89501276+Varao-Br@users.noreply.github.com>
2021-09-17 17:06:22 -0300

    First refactoraction.
    Created function int requestQuantity(void);

--- Geradordeprimos.c
+++ Geradordeprimos.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
+int requestQuantity(void);
+
 int main ()
 {
     setlocale(LC_ALL, "Portuguese");
-    int n, ultipri = 1, x = 1, prcont = 0;
-    printf("Quantos números primos você quer que sejam exibidos: ");
-    scanf("%d", &n);
-    int primo[n - 1];
+    int quantity, ultipri = 1, x = 1, prcont = 0;
+    quantity = requestQuantity();
+    int primo[quantity];
     primo[0] = 2;
     primo[1] = 3;
     printf("%d\n", primo[0]);
     printf("%d\n", primo[1]);
-    while(ultipri != n - 1)
+    while(ultipri != quantity - 1)
     {
         if (((primo[ultipri] + x) % primo[prcont]) == 0)
         {
@@ -36,4 +38,11 @@ int main ()
         }
     }
     return 0;
+}
+
+int requestQuantity(void){
+    int n;
+    printf("Quantos números primos você quer que sejam exibidos: ");
+    scanf("%d", &n);
+    return n;
 }
