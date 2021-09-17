cd985a4cb9e32b4a8b3b637927612311db9a1bde
Anderson André Ferreira Gomes <89501276+Varao-Br@users.noreply.github.com>
2021-09-17 02:32:44 -0300

    First commit

---
+++ Geradordeprimos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main ()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int n, ultipri = 1, x = 1, prcont = 0;
+    printf("Quantos números primos você quer que sejam exibidos: ");
+    scanf("%d", &n);
+    int primo[n - 1];
+    primo[0] = 2;
+    primo[1] = 3;
+    printf("%d\n", primo[0]);
+    printf("%d\n", primo[1]);
+    while(ultipri != n - 1)
+    {
+        if (((primo[ultipri] + x) % primo[prcont]) == 0)
+        {
+            prcont = 0;
+            x++;
+        }
+        else
+        {
+            if(prcont < ultipri)
+            {
+                prcont++;
+            }
+            else
+            {
+                primo[ultipri + 1] = primo[ultipri] + x;
+                ultipri++;
+                printf("%d\n", primo[ultipri]);
+                x = 1;
+                prcont = 0;
+
+            }
+        }
+    }
+    system("pause");
+    return 0;
+}
